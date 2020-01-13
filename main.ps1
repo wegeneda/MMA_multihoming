2d1e002cdab72804d530201d4b74c47c523a10c0
wegeneda <david@wegener.io>
2020-01-13 11:57:37 +0100

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -3,8 +3,11 @@ param(
   [string]$workspaceID,
   [Parameter(Mandatory = $true)]
   [string]$workspaceKey,
+  [Parameter(Mandatory = $false)]
+  [string]$ProxyURL
 )
 
 $mma = New-Object -ComObject 'AgentConfigManager.MgmtSvcCfg'
 $mma.AddCloudWorkspace($workspaceId, $workspaceKey)
+$mma.SetProxyUrl($ProxyURL)
 $mma.ReloadConfiguration()
